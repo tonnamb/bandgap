890d91d7f74b665c824370e324a1752c17ff4e0f
Tonnam Balankura <tonnam.balankura@gmail.com>
2016-10-09 00:26:26 -0400

    working json reading

--- js/bandgap.js
+++ js/bandgap.js
@@ -1,4 +1,3 @@
-var mplink = "https://www.materialsproject.org/rest/v1/materials/mp-971662/vasp?API_KEY=5g6ezwSkDu27cVqs"
-$.getJSON(mplink, function (data) {
-    console.log(mplink);
+$.getJSON("js/materials1.json", function(data) {
+    console.log(data[0]);
 });
